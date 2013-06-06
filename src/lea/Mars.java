ac676f77bebb16eb0dc30f5ffa5542b7fb127d81
rebelsky <rebelsky@grinnell.edu>
2013-06-06 15:22:05 -0500

    Created the first class.

---
+++ src/lea/Mars.java
@@ -0,0 +1,19 @@
+/**
+ * A sample class.  Whee!
+ */
+package lea;
+
+/**
+ * @author glimmerteam
+ *
+ */
+public class Mars {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world.");
+	}
+
+}
